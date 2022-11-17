321d3fda3aa55ee83f2bc55b6a7db5ef9179d961
Love Akinlesi <akinlesil@gmail.com>
2022-11-17 13:52:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Express on Vercel");
+    res.json({ message: "Express on Vercel" });
 });
 
 // Initialize server
